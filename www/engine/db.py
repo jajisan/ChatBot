c5fc4b1f681db286ddaf05bfc27d6d0a7a8710d4
jajisan <sandeepjaji.2001@gmail.com>
2025-01-20 00:29:38 +0530

    .

---
+++ www/engine/db.py
@@ -0,0 +1,11 @@
+import sqlite3
+
+conn = sqlite3.connect("jarvis.db")
+cursor = conn.cursor()
+
+query = "CREATE TABLE IF NOT EXISTS sys_command(id integer primary key , name VARCHAR(100),path VARCHAR(100)) "
+cursor.execute(query)
+
+query = "INSERT INTO sys_command VALUES(null,'','')"
+cursor.execute(query)
+conn.commit()
